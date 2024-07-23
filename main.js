41a4892c3b478272667870da4eea8c7b966dd954
jbrhm <jrabraham1904@gmail.com>
2024-07-23 10:09:39 -0400

    Render wireframe torus

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innderWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({
-	canvas: documnet.querySelector('#bg'),
+	canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -15,3 +15,17 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ( 30 );
 
 renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
+
+
+scene.add( torus );
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+animate();
