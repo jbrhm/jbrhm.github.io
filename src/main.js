bbb620f0f17518a3fd6ed479e27d7c8a37724f66
jbrhm <jrabraham1904@gmail.com>
2024-07-23 20:41:23 -0400

    Github meshes

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ var zScale = 1.0;
 
 var testMesh;
 
-loader.load( 'meshes/test.glb', function ( gltf ) {
+loader.load( '//github.com/jbrhm/jbrhm.github.io/raw/main/meshes/test.glb', function ( gltf ) {
 
 	testMesh = gltf.scene;
 
