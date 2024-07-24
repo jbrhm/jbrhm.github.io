fefcc25afefb1a7c91aa172ad2cbda1b36f6e895
jbrhm <jrabraham1904@gmail.com>
2024-07-23 20:23:31 -0400

    Better usage

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-
+// Init Scene //
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -20,11 +21,37 @@ camera.position.setZ( 30 );
 
 renderer.render( scene, camera );
 
+// Init Geometery //
+
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
 const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add( torus );
+
+// Load Assets
+
+const loader = new GLTFLoader();
+
+var xScale = 1.0;
+var yScale = 1.0;
+var zScale = 1.0;
+
+var testMesh;
+
+loader.load( 'meshes/test.glb', function ( gltf ) {
+
+	testMesh = gltf.scene;
+
+	scene.add( testMesh );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+// Init Lights //
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
@@ -40,6 +67,10 @@ function animate() {
 	torus.rotation.x += 0.01;
 	torus.rotation.y += 0.005;
 	torus.rotation.z += 0.01;
+
+	yScale += 0.01;
+
+	testMesh.scale.set(xScale, yScale, zScale);
 
 	controls.update();
 
