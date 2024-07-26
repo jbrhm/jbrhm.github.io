96f54cf3d95c657e0dce6d02ce3b5b63c07c037e
jbrhm <jrabraham1904@gmail.com>
2024-07-25 20:36:00 -0400

    Mars

--- src/main.js
+++ src/main.js
@@ -33,12 +33,6 @@ scene.add(axesHelper);
 
 // Init Geometery //
 
-function loadMesh(url) {
-	return new Promise((resolve, reject) => {
-		
-	});
-}
-
 var time = 0;
 var grid = new Grid(20, 20, new THREE.Vector3(-100,0,0), scene);
 
@@ -54,23 +48,57 @@ loader.load('https://raw.githubusercontent.com/jbrhm/jbrhm.github.io/e1d057416af
 	reject(error);
 });
 
+const moonTexture = new THREE.TextureLoader().load('resources/mars.jpeg');
+const normalTexture = new THREE.TextureLoader().load('resources/normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+moon.position.set(-200,75, 10);
+moon.scale.set(5, 5, 5);
+
+
 // Init Lights //
 
-const pointLight = new THREE.PointLight( 0xffffff );
-pointLight.position.set( 5, 5, 5 );
+const pointLight = new THREE.PointLight( 0x8f2f06, 1000, 1000 );
+pointLight.position.set(0, 65, 30);
 
-const ambientLight = new THREE.AmbientLight( 0xffffff );
+const lightHelper =  new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
+
+const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
 scene.add( pointLight, ambientLight );
 
 // Scrolling //
 
 function moveCamera() {
-	const t = document.body.getBoundingClientRect().top;
+	const t = Math.abs(document.body.getBoundingClientRect().top);
+
+	// Camera Scroll Movement
 	if(Math.abs(t) < 2000){
 		camera.position.z = -1 * Math.abs(t/250) ** 2 + 75;
 		camera.position.y = 0.1 * 1.003 ** Math.abs(t) + 2;
 
 		camera.rotation.x = 0.4 * Math.abs(t/1000) ** 2 + Math.PI/20;
+	}else if(t > 2000 && t < 3000){
+		camera.position.z = -1 * Math.abs(2000/250) ** 2 + 75;
+		camera.position.y = 0.1 * 1.003 ** Math.abs(2000) + 2;
+
+		camera.rotation.x = 0.4 * Math.abs(2000/1000) ** 2 + Math.PI/20;
+	}
+
+	// Mars Scroll Movement
+	if(t > 1500 && t < 2000){
+		moon.position.set(200 * ((t-1500)/500) - 225, 75, 10);
+	}else if(t > 2000 && t < 3000){
+		moon.position.set(-25, 75, 10);
+		moon.rotation.set(0, 0, (t - 2000) / 300)
 	}
 
 	console.log(t);
